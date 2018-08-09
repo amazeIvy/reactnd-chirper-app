0cb3a1fe9a69e2fd268c64fdc1120a4698cbec12
ivyfu77 <ivy.fu@vendhq.com>
2018-08-10 01:15:03 +1200

    Add createStore in root index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
-import tweets from './tweest'
+import tweets from './tweets'
 
 export default combineReducers({
   authedUser,
